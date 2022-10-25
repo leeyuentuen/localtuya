a398f0283ec832bbdc8a52b78859694b03fc9933
Tuen Lee <leeyuentuen@gmail.com>
2022-10-25 14:59:56 +0000

    rollback pack_message signing data

--- custom_components/localtuya/pytuya/__init__.py
+++ custom_components/localtuya/pytuya/__init__.py
@@ -49,10 +49,9 @@ import logging
 import struct
 import time
 import weakref
-import hmac
 from abc import ABC, abstractmethod
 from collections import namedtuple
-from hashlib import md5, sha256
+from hashlib import md5
 
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
@@ -78,11 +77,15 @@ _LOGGER = logging.getLogger(__name__)
 TuyaMessage = namedtuple("TuyaMessage", "seqno cmd retcode payload crc crcpassed")
 
 ACTION_SET = "set"
-ACTION_STATUS = "8"
+ACTION_STATUS = "status"
 ACTION_HEARTBEAT = "heartbeat"
 ACTION_UPDATEDPS = "updatedps"  # Request refresh of DPS
 ACTION_RESET = "reset"
 
+PROTOCOL_VERSION_BYTES_31 = b"3.1"
+PROTOCOL_VERSION_BYTES_33 = b"3.3"
+
+PROTOCOL_33_HEADER = PROTOCOL_VERSION_BYTES_33 + 12 * b"\x00"
 
 MESSAGE_HEADER_FMT = ">4I"  # 4*uint32: prefix, seqno, cmd, length
 MESSAGE_RECV_HEADER_FMT = ">5I"  # 4*uint32: prefix, seqno, cmd, length, retcode
@@ -107,73 +110,8 @@ COMMAND_HEARTBEAT = 0x09
 COMMAND_DP_QUERY_NEW = 0x10
 COMMAND_UPDATE_DPS = 0x12
 
-# Tuya Command Types
-# Reference: https://github.com/tuya/tuya-iotos-embeded-sdk-wifi-ble-bk7231n/blob/master/sdk/include/lan_protocol.h
-AP_CONFIG = 1  # FRM_TP_CFG_WF      # only used for ap 3.0 network config
-ACTIVE = 2  # FRM_TP_ACTV (discard) # WORK_MODE_CMD
-SESS_KEY_NEG_START = 3  # FRM_SECURITY_TYPE3 # negotiate session key
-SESS_KEY_NEG_RESP = 4  # FRM_SECURITY_TYPE4 # negotiate session key response
-SESS_KEY_NEG_FINISH = 5  # FRM_SECURITY_TYPE5 # finalize session key negotiation
-UNBIND = 6  # FRM_TP_UNBIND_DEV  # DATA_QUERT_CMD - issue command
-CONTROL = 7  # FRM_TP_CMD         # STATE_UPLOAD_CMD
-STATUS = 8  # FRM_TP_STAT_REPORT # STATE_QUERY_CMD
-HEART_BEAT = 9  # FRM_TP_HB
-DP_QUERY = 0x0A  # 10 # FRM_QUERY_STAT      # UPDATE_START_CMD - get data points
-QUERY_WIFI = 0x0B  # 11 # FRM_SSID_QUERY (discard) # UPDATE_TRANS_CMD
-TOKEN_BIND = 0x0C  # 12 # FRM_USER_BIND_REQ   # GET_ONLINE_TIME_CMD - system time (GMT)
-CONTROL_NEW = 0x0D  # 13 # FRM_TP_NEW_CMD      # FACTORY_MODE_CMD
-ENABLE_WIFI = 0x0E  # 14 # FRM_ADD_SUB_DEV_CMD # WIFI_TEST_CMD
-WIFI_INFO = 0x0F  # 15 # FRM_CFG_WIFI_INFO
-DP_QUERY_NEW = 0x10  # 16 # FRM_QUERY_STAT_NEW
-SCENE_EXECUTE = 0x11  # 17 # FRM_SCENE_EXEC
-UPDATEDPS = 0x12  # 18 # FRM_LAN_QUERY_DP    # Request refresh of DPS
-UDP_NEW = 0x13  # 19 # FR_TYPE_ENCRYPTION
-AP_CONFIG_NEW = 0x14  # 20 # FRM_AP_CFG_WF_V40
-BOARDCAST_LPV34 = 0x23  # 35 # FR_TYPE_BOARDCAST_LPV34
-LAN_EXT_STREAM = 0x40  # 64 # FRM_LAN_EXT_STREAM
-
-NO_PROTOCOL_HEADER_CMDS = [
-    DP_QUERY,
-    DP_QUERY_NEW,
-    UPDATEDPS,
-    HEART_BEAT,
-    SESS_KEY_NEG_START,
-    SESS_KEY_NEG_RESP,
-    SESS_KEY_NEG_FINISH,
-]
-
-
-# PROTOCOL_VERSION_BYTES_31 = b"3.1"
-# PROTOCOL_VERSION_BYTES_33 = b"3.3"
-
-# PROTOCOL_33_HEADER = PROTOCOL_VERSION_BYTES_33 + 12 * b"\x00"
-
-
-# Protocol Versions and Headers
 PROTOCOL_VERSION_BYTES_31 = b"3.1"
 PROTOCOL_VERSION_BYTES_33 = b"3.3"
-PROTOCOL_VERSION_BYTES_34 = b"3.4"
-PROTOCOL_3x_HEADER = 12 * b"\x00"
-PROTOCOL_33_HEADER = PROTOCOL_VERSION_BYTES_33 + PROTOCOL_3x_HEADER
-PROTOCOL_34_HEADER = PROTOCOL_VERSION_BYTES_34 + PROTOCOL_3x_HEADER
-MESSAGE_HEADER_FMT = ">4I"  # 4*uint32: prefix, seqno, cmd, length [, retcode]
-MESSAGE_RETCODE_FMT = ">I"  # retcode for received messages
-MESSAGE_END_FMT = ">2I"  # 2*uint32: crc, suffix
-MESSAGE_END_FMT_HMAC = ">32sI"  # 32s:hmac, uint32:suffix
-PREFIX_VALUE = 0x000055AA
-PREFIX_BIN = b"\x00\x00U\xaa"
-SUFFIX_VALUE = 0x0000AA55
-SUFFIX_BIN = b"\x00\x00\xaaU"
-
-NO_PROTOCOL_HEADER_CMDS = [
-    DP_QUERY,
-    DP_QUERY_NEW,
-    UPDATEDPS,
-    HEART_BEAT,
-    SESS_KEY_NEG_START,
-    SESS_KEY_NEG_RESP,
-    SESS_KEY_NEG_FINISH,
-]
 
 
 # This is intended to match requests.json payload at
@@ -284,27 +222,8 @@ class ContextualLogger:
         return self._logger.exception(msg, *args)
 
 
-def pack_message(msg, hmac_key=None):
-    # """Pack a TuyaMessage into bytes."""
-    # # Create full message excluding CRC and suffix
-    # buffer = (
-    #     struct.pack(
-    #         MESSAGE_HEADER_FMT,
-    #         PREFIX_VALUE,
-    #         msg.seqno,
-    #         msg.cmd,
-    #         len(msg.payload) + struct.calcsize(MESSAGE_END_FMT),
-    #     )
-    #     + msg.payload
-    # )
-
-    # # Calculate CRC, add it together with suffix
-    # buffer += struct.pack(MESSAGE_END_FMT, binascii.crc32(buffer), SUFFIX_VALUE)
-
-    # return buffer
-
+def pack_message(msg):
     """Pack a TuyaMessage into bytes."""
-    end_fmt = MESSAGE_END_FMT_HMAC if hmac_key else MESSAGE_END_FMT
     # Create full message excluding CRC and suffix
     buffer = (
         struct.pack(
@@ -312,16 +231,14 @@ def pack_message(msg, hmac_key=None):
             PREFIX_VALUE,
             msg.seqno,
             msg.cmd,
-            len(msg.payload) + struct.calcsize(end_fmt),
+            len(msg.payload) + struct.calcsize(MESSAGE_END_FMT),
         )
         + msg.payload
     )
-    if hmac_key:
-        crc = hmac.new(hmac_key, buffer, sha256).digest()
-    else:
-        crc = binascii.crc32(buffer) & 0xFFFFFFFF
+
     # Calculate CRC, add it together with suffix
-    buffer += struct.pack(end_fmt, crc, SUFFIX_VALUE)
+    buffer += struct.pack(MESSAGE_END_FMT, binascii.crc32(buffer), SUFFIX_VALUE)
+
     return buffer
 
 
